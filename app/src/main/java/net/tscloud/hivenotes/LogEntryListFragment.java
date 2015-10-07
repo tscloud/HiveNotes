18d67067cf16867734b6edcff8caab303f35c3e0
tscloud <thomas.s.cloud@gmail.com>
2015-10-06 23:37:13 -0400

    Background image to LogEntryList.

--- app/src/main/java/net/tscloud/hivenotes/LogEntryListFragment.java
+++ app/src/main/java/net/tscloud/hivenotes/LogEntryListFragment.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -77,6 +79,14 @@ public class LogEntryListFragment extends ListFragment {
                 LogEntryNames.ITEMS));
         */
         setListAdapter(new LogListAdapter(getActivity(), LogEntryNames.ITEMS));
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_log_entry_list, container, false);
+
+        return view;
     }
 
     @Override
