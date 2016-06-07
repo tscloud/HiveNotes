67541ce1b30e019b75ad77f031355cf5e52c6e33
tscloud <thomas.s.cloud@gmail.com>
2016-06-07 15:15:18 -0400

    Code up expandablerecyclerview. Get it to compile then test.

--- app/src/main/java/net/tscloud/hivenotes/LogDateListFragment.java
+++ app/src/main/java/net/tscloud/hivenotes/LogDateListFragment.java
@@ -21,12 +21,17 @@ public class LogDateListFragment extends Fragment {
 
     public static final String TAG = "LogDateListFragment";
 
+    // need Hive to read each Log table
     private long mHiveID;
 
+    // reference to Activity that should have started me
     private OnLogDateListFragmentListener mListener;
 
     // task references - needed to kill tasks on Fragment Destroy
     private GetDatesTask mTask = null;
+
+    // the RecyclerView
+    private RecyclerView mLogDateRecyclerView;
 
     /**
      * Use this factory method to create a new instance of
@@ -62,31 +67,34 @@ public class LogDateListFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_log_date_list, container, false);
 
+        mLogDateRecyclerView = (RecyclerView) view.findViewById(R.id.logdate_recycler_view);
+        mLogDateRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
         /** AsyncTask to get the date list - this task will be the only one to Load
          *   the list so no need to check for previous data, etc.
          */
         mTask = new GetDatesTask(getActivity());
 
         // set button listener
-        final Button b1 = (Button)v.findViewById(R.id.btnFragLogDateList);
+        //final Button b1 = (Button)v.findViewById(R.id.btnFragLogDateList);
 
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onButtonPressed();
-            }
-        });
+        //b1.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View v) {
+        //        onButtonPressed();
+        //    }
+        //});
 
         return v;
     }
 
-    public void onButtonPressed() {
-        Log.d(TAG, "in onButtonPressed");
+    //public void onButtonPressed() {
+    //    Log.d(TAG, "in onButtonPressed");
 
-        Calendar calendar = new GregorianCalendar(2013,0,31);
+    //    Calendar calendar = new GregorianCalendar(2013,0,31);
 
-        mListener.onLogDateListFragmentInteraction(calendar.getTimeInMillis());
-    }
+    //    mListener.onLogDateListFragmentInteraction(calendar.getTimeInMillis());
+    //}
 
     @Override
     public void onAttach(Activity activity) {
@@ -109,6 +117,7 @@ public class LogDateListFragment extends Fragment {
     public void onSaveInstanceState(Bundle outState) {
         // save off values potentially entered from screen
         super.onSaveInstanceState(outState);
+        ((LogDateExpandableAdapter) mLogDateRecyclerView.getAdapter()).onSaveInstanceState(outState);
     }
 
     @Override
@@ -124,9 +133,103 @@ public class LogDateListFragment extends Fragment {
         public void onLogDateListFragmentInteraction(long logDate);
     }
 
+    /** Stuff needed by expandablerecyclerview
+     */
+    // Parent view holder
+    public class LogDateParentViewHolder extends ParentViewHolder {
+
+        public TextView mLogDateTitleTextView;
+        public ImageButton mParentDropDownArrow;
+
+        public LogDateParentViewHolder(View itemView) {
+            super(itemView);
+
+            mLogDateTitleTextView = (TextView) itemView.findViewById(R.id.parent_list_item_logdate_title_text_view);
+            mParentDropDownArrow = (ImageButton) itemView.findViewById(R.id.parent_list_item_expand_arrow);
+        }
+    }
+
+    // Child view holder
+    public class LogDateChildViewHolder extends ChildViewHolder {
+
+        public TextView mLogDateDateText;
+
+        public LogDateChildViewHolder(View itemView) {
+            super(itemView);
+
+            mLogDateDateText = (TextView) itemView.findViewById(R.id.child_list_item_logdate_date_text_view);
+        }
+    }
+
+    // Parent object
+    public class LogDateParent implements ParentObject {
+
+        private List<Object> mChildrenList;
+
+        public LogDateParent() {
+        }
+
+        @Override
+        public List<Object> getChildObjectList() {
+            return mChildrenList;
+        }
+
+        @Override
+        public void setChildObjectList(List<Object> list) {
+            mChildrenList = list;
+        }
+    }
+
+    // Child object
+    public class LogDateChild {
+
+        private Date mDate;
+
+        public LogDateChild(Date date) {
+            mDate = date;
+        }
+
+        // Getter and setter methods
+    }
+
+    // Adapter
+    public class LogDateExpandableAdapter extends ExpandableRecyclerAdapter<LogDateParentViewHolder, LogDateChildViewHolder> {
+
+        private LayoutInflater mInflater;
+
+        public LogDateExpandableAdapter(Context context, List<ParentListItem> itemList) {
+            super(itemList);
+            mInflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public LogDateParentViewHolder onCreateParentViewHolder(ViewGroup viewGroup) {
+            View view = mInflater.inflate(R.layout.list_item_logdate_parent, viewGroup, false);
+            return new LogDateParentViewHolder(view);
+        }
+
+        @Override
+        public LogDateChildViewHolder onCreateChildViewHolder(ViewGroup viewGroup) {
+            View view = mInflater.inflate(R.layout.list_item_logdate_child, viewGroup, false);
+            return new LogDateChildViewHolder(view);
+        }
+
+        @Override
+        public void onBindParentViewHolder(LogDateParentViewHolder logDateParentViewHolder, int i, ParentListItem parentListItem) {
+            LogDateParent logDateParent = (LogDateParent) parentListItem;
+            logDateParentViewHolder.mLogDateTitleTextView.setText(logDateParent.getTitle());
+        }
+
+        @Override
+        public void onBindChildViewHolder(LogDateChildViewHolder logDateChildViewHolder, int i, Object childListItem) {
+            LogDateChild logDateChild = (LogDateChild) childListItem;
+            LogDateChildViewHolder.mLogDateDateText.setText(logDateChild.getDate().toString());
+        }
+    }
+
     /** get the list of dates we will present to the user
      */
-    public class GetDatesTask extends AsyncTask<Void, Void, Void> {
+    public class GetDatesTask extends AsyncTask<Void, Void, Long> {
 
         public static final String TAG = "GetDatesTask";
 
@@ -141,17 +244,43 @@ public class LogDateListFragment extends Fragment {
         protected Void doInBackground(Void... unused) {
             Log.d(TAG, "GetDatesTask("+ Thread.currentThread().getId() + ") : doInBackground");
 
+            LogDateDAO logDateDAO = new LogDateDAO();
+            List<Long> logDateList = logDateDAO.getAllVisitDates(mHiveID);
+
             return(null);
         }
 
         @Override
-        protected void onPostExecute(Void unused) {
+        protected void onPostExecute(Long dateArray) {
             Log.d(TAG, "UpdateDBTask("+ Thread.currentThread().getId() + ") : onPostExecute");
 
-            //Toast.makeText(getApplicationContext(), "DB update complete", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(getApplicationContext(), "DB query complete", Toast.LENGTH_SHORT).show();
+
+            LogDateExpandableAdapter logDateExpandableAdapter =
+                new LogDateExpandableAdapter(getActivity(), getLogDateParents(dateArray));
+            logDateExpandableAdapter.setCustomParentAnimationViewId(R.id.parent_list_item_expand_arrow);
+            logDateExpandableAdapter.setParentClickableViewAnimationDefaultDuration();
+            logDateExpandableAdapter.setParentAndIconExpandOnClick(true);
+
+            logDateExpandableAdapter.onRestoreInstanceState(savedInstanceState);
+
+            mLogDateRecyclerView.setAdapter(logDateExpandableAdapter);
 
             // all we need to do is nullify the Task reference
             mTask = null;
+        }
+
+        private ArrayList<ParentObject> getLogDateParents(List<Long> aLogDateList) {
+            // For each Date (Child object) => determine year/month, if
+            //  corresponding year/month Parent object does not exist => create
+            //  it and place this Child under it
+            ArrayList<ParentObject> reply = null;
+
+            for (long logDate : aLogDateList) {
+
+            }
+
+            return reply;
         }
     }
 }
---
+++ app/src/main/java/net/tscloud/hivenotes/db/LogDateDAO.java
@@ -0,0 +1,76 @@
+package net.tscloud.hivenotes.db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+/**
+ * Created by tscloud on 8/15/15.
+ */
+public class LogDateDAO {
+
+    public static final String TAG = "LogDateDAO";
+
+    // Database fields
+    private SQLiteDatabase mDatabase;
+    private MyDBHandler mDbHelper;
+    private Context mContext;
+
+    // compile time constant for big union query
+    private static final String q = "SELECT VISIT_DATE FROM LogEntryGeneral " +
+                                    "WHERE HIVE = ? " +
+                                    "UNION " +
+                                    "SELECT VISIT_DATE FROM LogEntryProductivity " +
+                                    "WHERE HIVE = ? " +
+                                    "UNION " +
+                                    "SELECT VISIT_DATE FROM LogEntryPestMgmt " +
+                                    "WHERE HIVE = ? " +
+                                    "UNION " +
+                                    "SELECT VISIT_DATE FROM LogEntryFeeding " +
+                                    "WHERE HIVE = ? " +
+                                    "UNION " +
+                                    "SELECT VISIT_DATE FROM LogEntryOther";
+                                    "WHERE HIVE = ?";
+
+    public LogDateDAO(Context context) {
+        this.mContext = context;
+        mDbHelper = MyDBHandler.getInstance(context);
+        // open the database
+        try {
+            open();
+        } catch (SQLException e) {
+            Log.e(TAG, "SQLException on openning database " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void open() throws SQLException {
+        mDatabase = mDbHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        mDbHelper.close();
+    }
+
+    // --DB access methods--
+
+    public List<Long> getAllVisitDates(long aHiveId) {
+        List<Long> listLogDate = null;
+        Cursor cursor = mDatabase.rawQuery(q, new String[] {aHiveId, aHiveId, aHiveId, aHiveId, aHiveId});
+
+        if (cursor != null) {
+            listLogDate = new ArrayList<Long>(cursor.getCount());
+            if (cursor.moveToFirst()) {
+                while (!cursor.isAfterLast()) {
+                    listHive.add(cursor.getLong(0));
+                    cursor.moveToNext();
+                }
+            }
+            // make sure to close the cursor
+            cursor.close();
+        }
+
+        return listHive;
